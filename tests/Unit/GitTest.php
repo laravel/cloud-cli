217f95e25712f2a6098e564ca125f8a9a27c6120
Mateus Guimarães <mateus.jatene@gmail.com>
2026-02-27 18:45:53 -0300

    Update GitTest.php

--- tests/Unit/GitTest.php
+++ tests/Unit/GitTest.php
@@ -3,66 +3,19 @@
 use App\Git;
 use Illuminate\Support\Facades\Process;
 
-it('parses SSH remote URLs', function () {
+it('parses remote URLs', function (string $remoteUrl, string $expected) {
     Process::fake([
-        '*git*remote*get-url*origin*' => Process::result('git@github.com:user/repo.git'),
+        '*git*remote*get-url*origin*' => Process::result($remoteUrl),
     ]);
 
-    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
-});
-
-it('parses HTTPS remote URLs', function () {
-    Process::fake([
-        '*git*remote*get-url*origin*' => Process::result('https://github.com/user/repo.git'),
-    ]);
-
-    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
-});
-
-it('parses HTTPS remote URLs without .git suffix', function () {
-    Process::fake([
-        '*git*remote*get-url*origin*' => Process::result('https://github.com/user/repo'),
-    ]);
-
-    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
-});
-
-it('parses SSH remote URLs without .git suffix', function () {
-    Process::fake([
-        '*git*remote*get-url*origin*' => Process::result('git@github.com:user/repo'),
-    ]);
-
-    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
-});
-
-it('parses GitLab SSH remote URLs', function () {
-    Process::fake([
-        '*git*remote*get-url*origin*' => Process::result('git@gitlab.com:user/repo.git'),
-    ]);
-
-    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
-});
-
-it('parses GitLab HTTPS remote URLs', function () {
-    Process::fake([
-        '*git*remote*get-url*origin*' => Process::result('https://gitlab.com/user/repo.git'),
-    ]);
-
-    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
-});
-
-it('parses Bitbucket SSH remote URLs', function () {
-    Process::fake([
-        '*git*remote*get-url*origin*' => Process::result('git@bitbucket.org:user/repo.git'),
-    ]);
-
-    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
-});
-
-it('parses Bitbucket HTTPS remote URLs', function () {
-    Process::fake([
-        '*git*remote*get-url*origin*' => Process::result('https://bitbucket.org/user/repo.git'),
-    ]);
-
-    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
-});
+    expect(app(Git::class)->remoteRepo())->toBe($expected);
+})->with([
+    'SSH with .git' => ['git@github.com:user/repo.git', 'user/repo'],
+    'SSH without .git' => ['git@github.com:user/repo', 'user/repo'],
+    'HTTPS with .git' => ['https://github.com/user/repo.git', 'user/repo'],
+    'HTTPS without .git' => ['https://github.com/user/repo', 'user/repo'],
+    'GitLab SSH' => ['git@gitlab.com:user/repo.git', 'user/repo'],
+    'GitLab HTTPS' => ['https://gitlab.com/user/repo.git', 'user/repo'],
+    'Bitbucket SSH' => ['git@bitbucket.org:user/repo.git', 'user/repo'],
+    'Bitbucket HTTPS' => ['https://bitbucket.org/user/repo.git', 'user/repo'],
+]);
