e873fc12bbb5f3980df4aa8b5194ac93c4aac2b0
Mateus Guimarães <mateus.jatene@gmail.com>
2026-02-27 18:44:22 -0300

    handle other providers

--- tests/Unit/GitTest.php
+++ tests/Unit/GitTest.php
@@ -34,3 +34,35 @@ it('parses SSH remote URLs without .git suffix', function () {
 
     expect(app(Git::class)->remoteRepo())->toBe('user/repo');
 });
+
+it('parses GitLab SSH remote URLs', function () {
+    Process::fake([
+        '*git*remote*get-url*origin*' => Process::result('git@gitlab.com:user/repo.git'),
+    ]);
+
+    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
+});
+
+it('parses GitLab HTTPS remote URLs', function () {
+    Process::fake([
+        '*git*remote*get-url*origin*' => Process::result('https://gitlab.com/user/repo.git'),
+    ]);
+
+    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
+});
+
+it('parses Bitbucket SSH remote URLs', function () {
+    Process::fake([
+        '*git*remote*get-url*origin*' => Process::result('git@bitbucket.org:user/repo.git'),
+    ]);
+
+    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
+});
+
+it('parses Bitbucket HTTPS remote URLs', function () {
+    Process::fake([
+        '*git*remote*get-url*origin*' => Process::result('https://bitbucket.org/user/repo.git'),
+    ]);
+
+    expect(app(Git::class)->remoteRepo())->toBe('user/repo');
+});
