6772662b02dc27f3b59dcfa170b46398b52dd74a
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-03-05 08:27:06 +0100

    wip

---
+++ app/Commands/Tinker.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Commands;
+
+use App\Client\Requests\RunCommandRequestData;
+use App\Prompts\MonitorCommand;
+use App\Prompts\TinkerPrompt;
+
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\intro;
+use function Laravel\Prompts\spin;
+
+class Tinker extends BaseCommand
+{
+    protected $signature = 'tinker
+        {environment? : The environment ID or name}
+        {--editor : Open the code in the editor}';
+
+    protected $description = 'Tinker in your Laravel Cloud environment';
+
+    protected string $codeTmpFile;
+
+    protected $tmpFileLastModifiedAt;
+
+    public function handle()
+    {
+        $this->ensureClient();
+
+        intro('Tinker');
+
+        $environment = $this->resolvers()->environment()->include('application')->from($this->argument('environment'));
+
+        if ($this->option('editor')) {
+            info('Every time you save the file, the code will be executed.');
+        }
+
+        while (true) {
+            $code = $this->getCodeForCommand();
+
+            if (str_starts_with($code, '<?php')) {
+                $code = str_replace('<?php', '', $code);
+            }
+
+            $code = trim($code);
+
+            if ($code === '') {
+                continue;
+            }
+
+            $command = spin(fn () => $this->client->commands()->run(
+                new RunCommandRequestData(
+                    environmentId: $environment->id,
+                    command: 'php artisan tinker --execute='.escapeshellarg($code),
+                ),
+            ), 'Running...');
+
+            (new MonitorCommand(
+                fn (string $id) => $this->client->commands()->get($id),
+                $command,
+            ))->display();
+        }
+    }
+
+    protected function getCodeForCommand()
+    {
+        if ($this->option('editor')) {
+            return $this->openInEditor();
+        }
+
+        return (new TinkerPrompt)->prompt();
+    }
+
+    protected function openInEditor()
+    {
+        $this->codeTmpFile ??= $this->initTmpFile();
+        $this->tmpFileLastModifiedAt = filemtime($this->codeTmpFile);
+
+        while (filemtime($this->codeTmpFile) === $this->tmpFileLastModifiedAt) {
+            // info(filemtime($this->codeTmpFile));
+            usleep(100_000);
+        }
+
+        $this->tmpFileLastModifiedAt = filemtime($this->codeTmpFile);
+
+        return file_get_contents($this->codeTmpFile);
+    }
+
+    protected function initTmpFile(): string
+    {
+        $this->codeTmpFile = tempnam(sys_get_temp_dir(), 'laravel-cloud-tinker-');
+
+        file_put_contents($this->codeTmpFile, '<?php '.PHP_EOL.PHP_EOL);
+
+        exec('open vscode://file/'.$this->codeTmpFile.':3:1');
+
+        return $this->codeTmpFile;
+    }
+}
