ac0712c6dc59a4fc1165741a8880e19d82f1f79a
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-04-02 16:41:27 -0700

    Write resolver error JSON to stderr instead of stdout
    Agents parsing stdout for valid JSON responses would confuse error
    messages with success output. Errors now go to stderr and include
    an 'error' key for easy identification.

--- app/Resolvers/Resolver.php
+++ app/Resolvers/Resolver.php
@@ -55,7 +55,7 @@ abstract class Resolver
     protected function failAndExit(string $message): void
     {
         if (! $this->isInteractive) {
-            echo json_encode(['message' => $message]).PHP_EOL;
+            fwrite(STDERR, json_encode(['error' => true, 'message' => $message]).PHP_EOL);
 
             throw new CommandExitException(Command::FAILURE);
         }
@@ -77,7 +77,7 @@ abstract class Resolver
     protected function ensureInteractive(string $message): void
     {
         if (! $this->isInteractive) {
-            echo json_encode(['message' => $message]).PHP_EOL;
+            fwrite(STDERR, json_encode(['error' => true, 'message' => $message]).PHP_EOL);
 
             throw new CommandExitException(Command::FAILURE);
         }
