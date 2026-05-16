133c322dffe1661ad64f7fc56fbba3261023324c
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-05-16 12:59:27 -0700

    Autocomplete artisan commands in command:run prompt (#158)
    * Update CommandRun.php
    
    * autocomplete renderer

---
+++ app/Prompts/AutocompletePromptRenderer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Prompts;
+
+use App\Concerns\DrawsThemeBoxes;
+use App\Enums\TimelineSymbol;
+use Laravel\Prompts\AutoCompletePrompt;
+
+class AutoCompletePromptRenderer extends Renderer
+{
+    use DrawsThemeBoxes;
+
+    /**
+     * Render the text prompt.
+     */
+    public function __invoke(AutoCompletePrompt $prompt): string
+    {
+        $maxWidth = $prompt->terminal()->cols() - 6;
+
+        return match ($prompt->state) {
+            'submit' => $this
+                ->box(
+                    $this->dim($this->truncate($prompt->label, $prompt->terminal()->cols() - 6)),
+                    $this->truncate($prompt->value(), $maxWidth),
+                    symbol: TimelineSymbol::SUCCESS,
+                ),
+
+            'cancel' => $this
+                ->box(
+                    $this->truncate($prompt->label, $prompt->terminal()->cols() - 6),
+                    $this->strikethrough($this->dim($this->truncate($prompt->value() ?: $prompt->placeholder, $maxWidth))),
+                    color: 'red',
+                    symbol: TimelineSymbol::FAILURE,
+                )
+                ->error($prompt->cancelMessage),
+
+            'error' => $this
+                ->box(
+                    $this->truncate($prompt->label, $prompt->terminal()->cols() - 6),
+                    $prompt->valueWithCursor($maxWidth),
+                    color: 'yellow',
+                    symbol: TimelineSymbol::WARNING,
+                )
+                ->warning($this->truncate($prompt->error, $prompt->terminal()->cols() - 5)),
+
+            default => $this
+                ->box(
+                    $this->cyan($this->truncate($prompt->label, $prompt->terminal()->cols() - 6)),
+                    $prompt->valueWithCursor($maxWidth),
+                )
+                ->when(
+                    $prompt->hint,
+                    fn () => $this->hint($prompt->hint),
+                    fn () => $this->newLine(), // Space for errors
+                ),
+        };
+    }
+}
