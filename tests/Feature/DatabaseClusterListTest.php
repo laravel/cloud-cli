171eb042c4be0c004c28056a3afb647ec7c70610
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-09-01 16:07:04 -0700

    scope cluster schemas to their owning cluster

--- tests/Feature/DatabaseClusterListTest.php
+++ tests/Feature/DatabaseClusterListTest.php
@@ -69,3 +69,63 @@ it('reads schemas out of the databases include', function () {
         ['id' => 'schema-2', 'name' => 'second', 'createdAt' => '2024-01-15T12:00:00.000000Z'],
     ]);
 });
+
+it('scopes schemas to the cluster that owns them', function () {
+    $first = databaseClusterResponse([
+        'id' => 'db-1',
+        'attributes' => ['name' => 'first-cluster'],
+        'included' => [databaseSchemaResponse(['id' => 'schema-1', 'attributes' => ['name' => 'first']])],
+    ]);
+
+    $second = databaseClusterResponse([
+        'id' => 'db-2',
+        'attributes' => ['name' => 'second-cluster'],
+        'included' => [databaseSchemaResponse(['id' => 'schema-2', 'attributes' => ['name' => 'second']])],
+    ]);
+
+    MockClient::global([
+        GetOrganizationRequest::class => MockResponse::make(organizationResponse(), 200),
+        ListDatabaseClustersRequest::class => MockResponse::make([
+            'data' => [$first['data'], $second['data']],
+            'included' => [...$first['included'], ...$second['included']],
+            'links' => ['next' => null],
+        ], 200),
+    ]);
+
+    $exitCode = Artisan::call('database-cluster:list', [
+        '--json' => true,
+        '--no-interaction' => true,
+    ]);
+
+    expect($exitCode)->toBe(0);
+
+    $output = json_decode(Artisan::output(), true);
+
+    expect(collect($output[0]['schemas'])->pluck('name')->all())->toBe(['first']);
+    expect(collect($output[1]['schemas'])->pluck('name')->all())->toBe(['second']);
+});
+
+it('falls back to every included schema when the cluster has no relationships', function () {
+    $cluster = databaseClusterResponse();
+    unset($cluster['data']['relationships']);
+
+    MockClient::global([
+        GetOrganizationRequest::class => MockResponse::make(organizationResponse(), 200),
+        ListDatabaseClustersRequest::class => MockResponse::make([
+            'data' => [$cluster['data']],
+            'included' => $cluster['included'],
+            'links' => ['next' => null],
+        ], 200),
+    ]);
+
+    $exitCode = Artisan::call('database-cluster:list', [
+        '--json' => true,
+        '--no-interaction' => true,
+    ]);
+
+    expect($exitCode)->toBe(0);
+
+    $output = json_decode(Artisan::output(), true);
+
+    expect(collect($output[0]['schemas'])->pluck('name')->all())->toBe(['my_schema']);
+});
